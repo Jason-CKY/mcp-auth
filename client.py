584bd137766bada0d11780bb20b38ccec5cc4267
Jason Cheng <jasoncky96@gmail.com>
2025-06-24 19:14:17 +0800

    custom header auth

--- client.py
+++ client.py
@@ -1,20 +1,28 @@
 import asyncio
+import httpx
 from fastmcp import Client
 from fastmcp.utilities.mcp_config import MCPConfig, RemoteMCPServer
 
-"""
-"""
+# this will be replaced with our internal auth from internal-llm-tools
+class CustomAuth(httpx.Auth):
+    def __init__(self, token: str):
+        self.token = token
+
+    def auth_flow(self, request):
+        request.headers["X-API-Key"] = self.token
+        yield request
 
 client = Client(
     MCPConfig(
         mcpServers={
-            "calendar": RemoteMCPServer(
+            "calendar_noauth": RemoteMCPServer(
                 url="http://localhost:8000/mcp/",
                 transport="streamable-http"
             ),
-            "authenticated calendar": RemoteMCPServer(
+            "calendar_auth": RemoteMCPServer(
                 url="http://localhost/mcp/",
                 transport="streamable-http",
+                auth=CustomAuth(token="secret-key")
             ),
         }
     )
